d267a3221d135ca7d53775bf8bd2107a258fe553
Krzysztof Marciniak <hun7err@gmail.com>
2014-04-28 10:58:51 +0200

    Poprawki w dokumentacji

--- Hikari Engine/RenderPass.h
+++ Hikari Engine/RenderPass.h
@@ -14,12 +14,18 @@ namespace Hikari {
 		public:
 			virtual RenderPass& run(std::list<RenderPass*> *renderTargets /** \brief wskaŸnik na listê celów (do ³¹czenia wielu etapów)*/); //< uruchamia proces renderingu, zwraca *this aby robiæ ci¹g wywo³añ
 
-			std::vector<ID3D11RenderTargetView*> * getRenderTargets();	//< pobiera listê celów
-			RenderPass& setRenderTargets(std::vector<ID3D11RenderTargetView*> * pRenderTargets);	//< ustawia now¹ listê celów
-			RenderPass& addRenderTarget(ID3D11RenderTargetView* pRenderTarget);	//< dodaje pojedynczy cel
-			RenderPass& removeRenderTarget(unsigned int index); //< Usuwa pojedynczy element z listy celów
+			/** \brief	Pobiera listê celów */
+			std::vector<ID3D11RenderTargetView*> * getRenderTargets();
+			/** \brief	Ustawia aktualn¹ listê celów */
+			RenderPass& setRenderTargets(std::vector<ID3D11RenderTargetView*> * pRenderTargets /** \brief wskaŸnik na now¹ listê celów */);
+			/** \brief	Dodaje pojedynczy cel */
+			RenderPass& addRenderTarget(ID3D11RenderTargetView* pRenderTarget /** \brief element do dodania */);
+			/** \brief	Usuwa pojedynczy element z listy celów */
+			RenderPass& removeRenderTarget(unsigned int index);
+
 
 			RenderPass();
+			/** \brief	Wirtualny destruktor m.in. zwalniajacy liste celow */
 			virtual ~RenderPass();
 		protected:
 			std::vector<ID3D11RenderTargetView*> * m_pRenderTargets;
