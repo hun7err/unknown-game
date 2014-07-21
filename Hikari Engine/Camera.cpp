9523a13922d554ca2dbeb51e0404b9b4679373fb
Krzysztof Marciniak <hun7err@gmail.com>
2014-07-21 20:12:27 +0200

    Szkielet klasy kamery

---
+++ Hikari Engine/Camera.cpp
@@ -0,0 +1,11 @@
+#include "Camera.hpp"
+
+DirectX::XMMATRIX Hikari::Camera::projectionMatrix(void)
+{
+	return m_ProjectionMatrix;
+}
+
+void Hikari::Camera::projectionMatrix(DirectX::XMMATRIX *newProjectionMatrix)
+{
+	m_ProjectionMatrix = *newProjectionMatrix;
+}
