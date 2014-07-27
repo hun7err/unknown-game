55d4601aa09bcfcf29f16847d7f2563fd40bc3f5
Krzysztof Marciniak <hun7err@gmail.com>
2014-07-27 14:39:35 +0200

    Revert "Naprawa warningow Doxygena"
    This reverts commit b3a47c70d50f2ed68a7c44c970341daaac04b6bd.

--- Hikari Engine/include/RenderPass.hpp
+++ Hikari Engine/include/RenderPass.hpp
@@ -15,18 +15,18 @@ namespace Hikari
 	class RenderPass
 	{
 		public:
-			virtual void run(RenderPass* pRenderTargets /** wskaŸnik na listê celów (do ³¹czenia wielu etapów)*/) = 0; //< uruchamia proces renderingu, zwraca *this aby robiæ ci¹g wywo³añ
+			virtual void run(RenderPass* pRenderTargets /** \brief wskaŸnik na listê celów (do ³¹czenia wielu etapów)*/) = 0; //< uruchamia proces renderingu, zwraca *this aby robiæ ci¹g wywo³añ
 
-			/** Pobiera listê celów */
+			/** \brief	Pobiera listê celów */
 			std::vector<std::pair<ID3D11RenderTargetView*, ID3D11Texture2D*> > * renderTargets(void);
 			std::vector<ID3D11RenderTargetView*> renderTargetViews(void);
 			std::pair<ID3D11RenderTargetView*, ID3D11Texture2D*> renderTarget(unsigned int index);
-			/** Ustawia aktualn¹ listê celów */
-			void renderTargets(std::vector<std::pair<ID3D11RenderTargetView*, ID3D11Texture2D*> > * pRenderTargets /** wskaŸnik na now¹ listê celów */);
-			/** Dodaje pojedynczy cel */
-			void addRenderTarget(ID3D11Texture2D* pTexture /** tekstura */, ID3D11RenderTargetView* pRenderTarget /** render target utworzony z tekstury */, D3D11_VIEWPORT renderTargetViewport);
-			/** Usuwa pojedynczy element z listy celów */
-			void removeRenderTarget(unsigned int index /** Indeks elementu, który ma zostaæ usuniêty */);
+			/** \brief	Ustawia aktualn¹ listê celów */
+			void renderTargets(std::vector<std::pair<ID3D11RenderTargetView*, ID3D11Texture2D*> > * pRenderTargets /** \brief wskaŸnik na now¹ listê celów */);
+			/** \brief	Dodaje pojedynczy cel */
+			void addRenderTarget(ID3D11Texture2D* pTexture /** \brief tekstura */, ID3D11RenderTargetView* pRenderTarget /** \brief render target utworzony z tekstury */, D3D11_VIEWPORT renderTargetViewport);
+			/** \brief	Usuwa pojedynczy element z listy celów */
+			void removeRenderTarget(unsigned int index /** \brief Indeks elementu, który ma zostaæ usuniêty */);
 			D3D11_VIEWPORT viewport(unsigned int index);
 			std::vector<D3D11_VIEWPORT> viewports(void);
 
@@ -35,7 +35,7 @@ namespace Hikari
 			void clearColor(DirectX::XMVECTORF32 *newClearColor);
 
 			RenderPass();
-			/** Wirtualny destruktor m.in. zwalniajacy liste celow */
+			/** \brief	Wirtualny destruktor m.in. zwalniajacy liste celow */
 			virtual ~RenderPass();
 
 		protected:
